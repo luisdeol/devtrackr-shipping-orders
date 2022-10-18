bdc0cdb12e1e8f0c595d39b7c02a3dd330a6679b
Luis Felipe <luis.mesa@authoritypartners.com>
2022-10-17 21:49:22 -0300

    Pré Aula 3

---
+++ src/DevTrackR.ShippingOrders.Infrastructure/Persistence/MongoDbOptions.cs
@@ -0,0 +1,8 @@
+namespace DevTrackR.ShippingOrders.Infrastructure.Persistence
+{
+    public class MongoDbOptions
+    {
+        public string ConnectionString { get; set; }
+        public string Database { get; set; }
+    }
+}
